3ac9f5c2e96e4ffda3a5d2a9715fc47c4cfbd3a6
Naresh Singh <nareshsingh0170@gmail.com>
2023-12-19 13:24:39 +0530

    JavaScript Done

--- js/app.js
+++ js/app.js
@@ -12,21 +12,21 @@ const symbolCheck = document.querySelector("#symbols");
 
 const indicator = document.querySelector("[data-indicator]");
 const generateBtn = document.querySelector(".generateBtn");
-const allCheckBox = document.querySelector("input[type=checkbox]");
+const allCheckBox = document.querySelectorAll("input[type=checkbox]");
 const symbols = '`~!@#$%^&*()_-+{}[]<>?/'
 
 let password = "";
 let passwordLength = 10;
 
-let checkCount = 1;
+let checkCount = 0;
 handalSlider()
 //set strength cricle color to gray
 
 //Set Password Length 
 function handalSlider() {
 
-    let a = inputSlider.value = passwordLength;
-    lengthDisplay.innerHTML = passwordLength;
+    inputSlider.value = passwordLength;
+    lengthDisplay.innerText = passwordLength;
     //or kuch bhi karna chahiye
 
 }
@@ -53,8 +53,6 @@ function generateLowerCase() {
 }
 
 function generateSymbol() {
-    // let randNum = Math.floor(Math.random() * symbols.length);
-
     let randNum = getRndInterger(0, symbols.length);
     return symbols.charAt(randNum);
 }
@@ -79,7 +77,103 @@ function calcStrength() {
     else {
         setIndicator("#f00")
     }
-
 }
 
-generateSymbol()
+async function copyContent() {
+    try {
+        await navigator.clipboard.writeText(passwordDisplay.value);
+        copyMsg.innerText = "Copied";
+    }
+    catch (err) {
+        copyMsg.innerText = "Failed";
+    }
+
+    copyMsg.classList.add("active");
+
+    setTimeout(() => {
+        copyMsg.classList.remove("active");
+    }, 2000)
+}
+
+function shufflePassword(array) {
+    //Fisher Yates Method
+    for (let i = array.length - 1; i > 0; i--) {
+        //random J, find out using random function
+        const j = Math.floor(Math.random() * (i + 1));
+        //swap number at i index and j index
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    let str = "";
+    array.forEach((el) => (str += el));
+    return str;
+}
+
+function handalCheckBoxChange() {
+    checkCount = 0;
+    allCheckBox.forEach((checkBox) => {
+        if (checkBox.checked) checkCount++;
+    });
+
+    if (passwordLength < checkCount) {
+        passwordLength = checkCount;
+        handalSlider();
+    }
+}
+
+allCheckBox.forEach((checkBox) => {
+    checkBox.addEventListener('change', handalCheckBoxChange)
+})
+
+inputSlider.addEventListener('input', (e) => {
+    passwordLength = e.target.value;
+    handalSlider();
+})
+
+copyBtn.addEventListener("click", () => {
+    if (passwordDisplay.value) copyContent();
+})
+
+generateBtn.addEventListener('click', () => {
+    if (checkCount == 0) return;
+
+    if (passwordLength < checkCount) {
+        passwordLength = checkCount;
+        handalSlider();
+    }
+
+    // Find New Password
+    // remove Old password
+    password = "";
+
+    let funArr = []
+
+    if (uppercaseCheck.checked) funArr.push(generateUpperCase);
+    if (lowercasecheck.checked) funArr.push(generateLowerCase);
+
+    if (numbercheck.checked) funArr.push(generateRandomNumber);
+    if (symbolCheck.checked) funArr.push(generateSymbol);
+
+    // Compulsory additions
+    for (let i = 0; i < funArr.length; i++) {
+        password += funArr[i]();
+    }
+
+    // remaining addition
+    for (let i = 0; i < passwordLength - funArr.length; i++) {
+        let randIndex = getRndInterger(0, funArr.length);
+        password += funArr[randIndex]();
+    }
+
+    // shuffle the password
+    password = shufflePassword(Array.from(password));
+
+    // show in the UI
+    passwordDisplay.value = password;
+
+    console.log(passwordDisplay.value = password);
+
+    // calculate strength
+    calcStrength();
+})
